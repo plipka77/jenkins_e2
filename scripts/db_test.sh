0ef96e852952220183d1344ffca82176400ace4b
Paul Lipka <plipka07@outlook.com>
2024-02-12 17:24:09 -0700

    Fix script

--- scripts/db_test.sh
+++ scripts/db_test.sh
@@ -2,7 +2,7 @@
 status=1
 while [ $status -ne 0 ]  
 do
-    mysqladmin ping --port 3333 -u root --password=Boomersooner7 2> /dev/null
+    mysqladmin ping --port 3333 -u root --password=Hello.WORLD88* 2> /dev/null
     status=$?
     echo "Database not started yet... waiting..."
     sleep 5
